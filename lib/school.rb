5d97e020dc8824d09874dfbaa259ede96b2669ba
Charles <charlesangeloplai@gmail.com>
2020-11-18 01:43:09 -0500

    commit tree 74ddc53246a5b92e00bee06d4a98b4c7dedb0be8

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,44 @@
 # code here!
+require "pry"
+
+class School
+    attr_accessor :name
+
+    def initialize(name)
+        @name = name
+        @roster = Hash.new
+    end
+
+    def roster
+        @roster
+    end
+
+    def add_student(student, grade)
+        if @roster.empty? || !@roster.has_key?(grade)
+            @roster[grade] = []
+            @roster[grade] << student
+        else
+            @roster[grade] << student
+        end
+    end
+
+    def grade(grade)
+        students = []
+
+        @roster.each do |key, values|
+            if key == grade
+                values.each do |value|
+                    students << value
+                end
+            end
+        end
+
+        students
+    end
+
+    def sort
+        @roster.sort.collect do |key, value|
+            [key, value.sort]
+        end.to_h
+    end
+end
